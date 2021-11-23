a58a6788127a01b894cde63d3186784022f464e6
onlylife59 <octonber@qq.com>
2021-11-23 16:44:31 +0800

    提升版本

--- packages/cra-template-component/template/src/components/Demo/Index.tsx
+++ packages/cra-template-component/template/src/components/Demo/Index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import Paper from '@mui/material/Paper';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
 
 import logo from "./assets/logo.svg";
 import styles from "./styles/Demo.module.css";
@@ -15,9 +15,9 @@ import { useTheme, getThemes, ThemeInfo } from '@v-act/styles';
  */
 function Demo() {
     const theme = useTheme();
-    const menus:Array<{code:string,title:string}> = [];
+    const menus: Array<{ code: string, title: string }> = [];
     const menusObj = getThemes();
-    menusObj.forEach((menu: ThemeInfo)=>{
+    menusObj.forEach((menu: ThemeInfo) => {
         menus.push({
             code: menu.getCode(),
             title: menu.getName()
@@ -30,34 +30,30 @@ function Demo() {
                     color: theme.vact.primaryColor,
                     backgroundColor: theme.vact.backgroundBaseColor
                 }}>
-                    <Grid container spacing={12}>
-                        <Grid item  xs={3} ></Grid>
-                        <Grid item  xs={6}>
-                            <Card style={{
-                                margin: '16px'
-                            }}>
-                                <CardContent>
-                                    <img src={logo} className={styles.logo} alt="logo" />
-                                    <h1>欢迎进行V平台React组件开发</h1>
-                                    <h6>这是你是天马行空的创作天地</h6>
-                                    <p>
-                                        编辑
-                                        <code>src/components/Demo/Index.tsx</code>
-                                        文件试试吧！
-                                    </p>
-                                    <a
-                                        className={styles.link}
-                                        href="https://reactjs.org"
-                                        target="_blank"
-                                        rel="noopener noreferrer"
-                                    >
-                                        开始学习
-                                    </a>
-                                </CardContent>
-                            </Card>
-                        </Grid>
-                        <Grid item  xs={3} ></Grid>
-                    </Grid>
+                    <Container>
+                        <Card style={{
+                            margin: '16px'
+                        }}>
+                            <CardContent>
+                                <img src={logo} className={styles.logo} alt="logo" />
+                                <h1>欢迎进行V平台React组件开发</h1>
+                                <h6>这是你是天马行空的创作天地</h6>
+                                <p>
+                                    编辑
+                                    <code>src/components/Demo/Index.tsx</code>
+                                    文件试试吧！
+                                </p>
+                                <a
+                                    className={styles.link}
+                                    href="https://reactjs.org"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    开始学习
+                                </a>
+                            </CardContent>
+                        </Card>
+                    </Container>
                 </div>
             </div>
         </Paper>
