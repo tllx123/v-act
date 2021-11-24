a0410fbb919830f720ecbb4e409ea46b62a499b0
onlylife59 <octonber@qq.com>
2021-11-24 14:04:38 +0800

    错误处理

--- packages/v-act-component-cli/build/v-act/v-act-config-overrides.js
+++ packages/v-act-component-cli/build/v-act/v-act-config-overrides.js
@@ -1,4 +1,5 @@
 const requireAll = require("require-all");
+const fs = require("fs");
 const extraOverride = require("../v-act-component-config-overrides");
 
 /**
@@ -6,19 +7,22 @@ const extraOverride = require("../v-act-component-config-overrides");
  * 注意：请不要调整此代码，如需添加webpack配置，请在v3-component-config-overrides.js中添加
  * */
 module.exports = function (config, env) {
-  let overrides = requireAll({
-    dirname: __dirname + "/overrides",
-    recursive: true
-  });
-  if (overrides) {
-    Object.values(overrides).forEach(function (handlers) {
-      handlers = Array.isArray(handlers) ? handlers : [handlers];
-      if (handlers && handlers.length > 0) {
-        handlers.forEach(handler => {
-          config = handler(config, env);
-        });
-      }
+  const path = __dirname + "/overrides";
+  if (fs.existsSync(path)) {
+    let overrides = requireAll({
+      dirname: path,
+      recursive: true
     });
+    if (overrides) {
+      Object.values(overrides).forEach(function (handlers) {
+        handlers = Array.isArray(handlers) ? handlers : [handlers];
+        if (handlers && handlers.length > 0) {
+          handlers.forEach(handler => {
+            config = handler(config, env);
+          });
+        }
+      });
+    }
   }
   config = extraOverride(config, env);
   return config;
