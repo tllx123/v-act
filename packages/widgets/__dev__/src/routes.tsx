324d7da422a22322cb734eff62a5e1f1b6bfc601
Chavy <chavyleung@gmail.com>
2021-12-06 15:36:23 +0800

    feat(widgets-dev): add JGImage

--- packages/widgets/__dev__/src/routes.tsx
+++ packages/widgets/__dev__/src/routes.tsx
@@ -1,6 +1,7 @@
 import { useRoutes } from 'react-router-dom'
 
 import JGButton from './widgets/JGButton'
+import JGImage from './widgets/JGImage'
 import JGLabel from './widgets/JGLabel'
 
 const Routes = () =>
@@ -12,6 +13,10 @@ const Routes = () =>
     {
       path: '/JGLabel',
       element: <JGLabel />
+    },
+    {
+      path: '/JGImage',
+      element: <JGImage />
     }
   ])
 
---
+++ packages/widgets/__dev__/src/widgets/JGImage/index.tsx
@@ -0,0 +1,13 @@
+import { JGComponent } from '@v-act/jgcomponent'
+import { JGImage } from '@v-act/jgimage'
+
+const Button = () => {
+  return (
+    <JGComponent>
+      <JGImage top="39px" left="72px" height="100px" width="100px"></JGImage>
+      <JGImage top="39px" left="247px" height="181px" width="327px"></JGImage>
+    </JGComponent>
+  )
+}
+
+export default Button
