f7633523ac3c936483938072fed4ce66955efc29
Chavy <chavyleung@gmail.com>
2021-12-06 16:22:50 +0800

    chore: update EOL

--- packages/cli/v-act-component-cli/bin/index.js
+++ packages/cli/v-act-component-cli/bin/index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-const spawn = require('react-dev-utils/crossSpawn');
-const utils = require('@v-act/utils');
+const spawn = require('react-dev-utils/crossSpawn')
+const utils = require('@v-act/utils')
 
-const args = process.argv.slice(2);
+const args = process.argv.slice(2)
 const scripts = [
   'build',
   'start',
@@ -14,39 +14,40 @@ const scripts = [
   'pack',
   'preinstall',
   'uninstall'
-];
+]
 
-const scriptIndex = args.findIndex(
-  x => scripts.indexOf(x) != -1
-);
+const scriptIndex = args.findIndex((x) => scripts.indexOf(x) != -1)
 
-const script = scriptIndex === -1 ? args[0] : args[scriptIndex];
-const nodeArgs = scriptIndex > 0 ? args.slice(0, scriptIndex) : [];
+const script = scriptIndex === -1 ? args[0] : args[scriptIndex]
+const nodeArgs = scriptIndex > 0 ? args.slice(0, scriptIndex) : []
 
 if (scripts.indexOf(script) != -1) {
   const result = spawn.sync(
     'node',
     nodeArgs
-    .concat(require.resolve('../scripts/' + script))
-    .concat(args.slice(scriptIndex + 1)), {
+      .concat(require.resolve('../scripts/' + script))
+      .concat(args.slice(scriptIndex + 1)),
+    {
       stdio: 'inherit'
     }
-  );
+  )
   if (result.signal) {
     if (result.signal === 'SIGKILL') {
-      utils.Console.debug('The build failed because the process exited too early. ' +
-        'This probably means the system ran out of memory or someone called ' +
-        '`kill -9` on the process.');
+      utils.Console.debug(
+        'The build failed because the process exited too early. ' +
+          'This probably means the system ran out of memory or someone called ' +
+          '`kill -9` on the process.'
+      )
     } else if (result.signal === 'SIGTERM') {
       utils.Console.debug(
         'The build failed because the process exited too early. ' +
-        'Someone might have called `kill` or `killall`, or the system could ' +
-        'be shutting down.'
-      );
+          'Someone might have called `kill` or `killall`, or the system could ' +
+          'be shutting down.'
+      )
     }
-    process.exit(1);
+    process.exit(1)
   }
-  process.exit(result.status);
+  process.exit(result.status)
 } else {
-  utils.Console.error('未知脚本："' + script + '"，请检查输入！');
+  utils.Console.error('未知脚本："' + script + '"，请检查输入！')
 }
