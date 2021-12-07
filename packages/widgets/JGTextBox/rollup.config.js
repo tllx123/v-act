5a53b9bed45494df355ad7b2cc45fdd749a3e08c
onlylife59 <octonber@qq.com>
2021-12-07 11:00:49 +0800

    添加打包排除项

--- packages/widgets/JGTextBox/rollup.config.js
+++ packages/widgets/JGTextBox/rollup.config.js
@@ -11,7 +11,7 @@ import pkg from './package.json'
 const extensions = ['.js', '.ts', '.tsx']
 
 const entryConfig = defineConfig({
-  external: ['react', 'react-dom', '@mui/material'],
+  external: ['react', 'react-dom', '@mui/material', '@v-act/schema-types'],
   input: 'src/index.tsx',
   output: [
     {
