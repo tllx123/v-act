150641b64bd839343d7dd65266ea8ccaad76f9e2
yzc <123@123.com>
2021-12-07 16:16:27 +0800

    merge

--- packages/widgets/__dev__/src/routes.tsx
+++ packages/widgets/__dev__/src/routes.tsx
@@ -1,14 +1,23 @@
 import { useRoutes } from 'react-router-dom'
 
+import Home from './widgets/Home'
 import JGButton from './widgets/JGButton'
 import JGImage from './widgets/JGImage'
 import JGLabel from './widgets/JGLabel'
+import JGNewList from './widgets/JGNewList'
+import JGPortal from './widgets/JGPortal'
+import JGRecordPaging from './widgets/JGRecordPaging'
+import JGSteps from './widgets/JGSteps'
 import JGTextBox from './widgets/JGTextBox'
 import JGFloatBox from './widgets/JGFloatBox'
 import JGPassword from './widgets/JGPassword'
 
 const Routes = () =>
   useRoutes([
+    {
+      path: '/',
+      element: <Home />
+    },
     {
       path: '/JGButton',
       element: <JGButton />
@@ -24,6 +33,22 @@ const Routes = () =>
     {
       path: '/JGTextBox',
       element: <JGTextBox />
+    },
+    {
+      path: '/JGNewList',
+      element: <JGNewList />
+    },
+    {
+      path: '/JGPortal',
+      element: <JGPortal />
+    },
+    {
+      path: '/JGRecordPaging',
+      element: <JGRecordPaging />
+    },
+    {
+      path: '/JGSteps',
+      element: <JGSteps />
     },
     {
       path: '/JGFloatBox',
