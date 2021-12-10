7e4c28cec90b7bc685be6f5481dfae0e7c48f686
Chavy <chavyleung@gmail.com>
2021-12-10 15:24:55 +0800

    chore(widgets-dev): revert routes.tsx

--- packages/widgets/__dev__/src/routes.tsx
+++ packages/widgets/__dev__/src/routes.tsx
@@ -1,17 +1,31 @@
 import { useRoutes } from 'react-router-dom'
 
+import Home from './widgets/Home'
+import JGAttachment from './widgets/JGAttachment'
 import JGButton from './widgets/JGButton'
-import JGImage from './widgets/JGImage'
-import JGLabel from './widgets/JGLabel'
-import JGNewList from './widgets/JGNewList'
-import JGRecordPaging from './widgets/JGRecordPaging'
-import JGTextBox from './widgets/JGTextBox'
-import JGLongTextBox from './widgets/JGLongTextBox'
 import JGCheckBox from './widgets/JGCheckBox'
 import JGDateTimePicker from './widgets/JGDateTimePicker'
+import JGFloatBox from './widgets/JGFloatBox'
+import JGImage from './widgets/JGImage'
+import JGIntegerBox from './widgets/JGIntegerBox'
+import JGLabel from './widgets/JGLabel'
 import JGLongDateTimePicker from './widgets/JGLongDateTimePicker'
+import JGLongTextBox from './widgets/JGLongTextBox'
+import JGNewList from './widgets/JGNewList'
+import JGPassword from './widgets/JGPassword'
+import JGPeriod from './widgets/JGPeriod'
+import JGPortal from './widgets/JGPortal'
+import JGRecordPaging from './widgets/JGRecordPaging'
+import JGSteps from './widgets/JGSteps'
+import JGTabControl from './widgets/JGTabControl'
+import JGTextBox from './widgets/JGTextBox'
+
 const Routes = () =>
   useRoutes([
+    {
+      path: '/',
+      element: <Home />
+    },
     {
       path: '/JGButton',
       element: <JGButton />
@@ -33,8 +47,40 @@ const Routes = () =>
       element: <JGNewList />
     },
     {
+      path: '/JGPortal',
+      element: <JGPortal />
+    },
+    {
       path: '/JGRecordPaging',
       element: <JGRecordPaging />
+    },
+    {
+      path: '/JGSteps',
+      element: <JGSteps />
+    },
+    {
+      path: '/JGFloatBox',
+      element: <JGFloatBox />
+    },
+    {
+      path: '/JGPassword',
+      element: <JGPassword />
+    },
+    {
+      path: '/JGIntegerBox',
+      element: <JGIntegerBox />
+    },
+    {
+      path: '/JGAttachment',
+      element: <JGAttachment />
+    },
+    {
+      path: '/JGPeriod',
+      element: <JGPeriod />
+    },
+    {
+      path: '/JGTabControl',
+      element: <JGTabControl />
     },
     {
       path: '/JGLongTextBox',
