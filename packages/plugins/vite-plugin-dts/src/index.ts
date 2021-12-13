2f6a35708ee90b78d468c734272bac5101b5a040
Chavy <chavyleung@gmail.com>
2021-12-13 11:06:31 +0800

    feat(vite-plugin-dts): create repo

---
+++ packages/plugins/vite-plugin-dts/src/index.ts
@@ -0,0 +1,20 @@
+import { Plugin } from 'vite'
+
+const vitePluginDts = () => {
+  const plugin: Plugin = {
+    name: 'vite:dts',
+    apply: 'build',
+    async generateBundle({ format }) {
+      if (format !== 'es') return
+      this.emitFile({
+        type: 'asset',
+        fileName: 'index.d.ts',
+        source: `export * from './../src'`
+      })
+    }
+  }
+
+  return plugin
+}
+
+export default vitePluginDts
