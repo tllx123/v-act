17abdfc0858392f672c1ec13acf46d5f3399bc62
Chavy <chavyleung@gmail.com>
2021-12-17 11:13:51 +0800

    fix: umd pack

--- scripts/build.js
+++ scripts/build.js
@@ -41,13 +41,13 @@ export async function viteBuild(scopes) {
   return await filteredPackages.map(async (pkg) => {
     const root = pkg.location
     const name = pkg.name
-    const fileName = () => 'index.js'
+    const fileName = (fmt) => (fmt === 'umd' ? 'index.js' : 'index.es.js')
     const entry = `${root}/src`
 
     return await build({
       build: {
         emptyOutDir: true,
-        lib: { entry, name, fileName, formats: ['es'] },
+        lib: { entry, name, fileName, formats: ['umd', 'es'] },
         rollupOptions: { external },
         sourcemap: true
       },
