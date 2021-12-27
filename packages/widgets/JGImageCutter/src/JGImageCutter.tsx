8c0d680749e64360ec1d2998859b6c4f6f708967
zhougege520 <2912354876@qq.com>
2021-12-27 15:13:45 +0800

    图片裁剪

--- packages/widgets/JGImageCutter/src/JGImageCutter.tsx
+++ packages/widgets/JGImageCutter/src/JGImageCutter.tsx
@@ -1,18 +1,24 @@
-import React, { CSSProperties } from 'react'
+import 'cropperjs/dist/cropper.css'
+
+import React, { CSSProperties, useRef } from 'react'
 
 import { Property } from 'csstype'
+import { Cropper } from 'react-cropper'
 
 import { Box, BoxProps } from '@mui/material'
+import { useContext } from '@v-act/widget-context'
 
 import logo from './img/blank.png'
 
 /* 包装器属性 */
-export interface JGImageCutterProps extends BoxProps {
+interface JGImageCutterProps extends BoxProps {
+  /************************** 其它 **************************************/
   /**
    * 编码
    */
   code?: string
 
+  /************************** 格式 **************************************/
   /**
    * 上边距
    */
@@ -83,11 +89,13 @@ export interface JGImageCutterProps extends BoxProps {
    */
   newImageObjId?: string
 
+  /************************** 事件 **************************************/
   /**
    * 裁剪完成事件
    */
   onSelected?: string
 
+  /************************** 数据 **************************************/
   /**
    * 实体名
    */
@@ -100,6 +108,9 @@ export interface JGImageCutterProps extends BoxProps {
 }
 
 const JGImageCutter = function (props: JGImageCutterProps) {
+  const context = useContext()
+  const cropperRef = useRef<HTMLImageElement>(null)
+
   /* 包装器样式 */
   const wrapStyles: CSSProperties = {
     width: props.width,
@@ -108,9 +119,16 @@ const JGImageCutter = function (props: JGImageCutterProps) {
     top: props.top,
     display: 'inline-flex',
     fontSize: '14px',
-    position: 'absolute',
+    position: context.position,
+    backgroundImage: `url(${logo})`,
     fontFamily:
       'Helvetica Neue,Helvetica,PingFang SC,Hiragino Sans GB,Microsoft YaHei,\\5FAE\\8F6F\\96C5\\9ED1,Arial,sans-serif'
+  }
+
+  const myCrop = function (a: any) {
+    const imageElement: any = cropperRef?.current
+    const cropper: any = imageElement?.cropper
+    console.log(cropper.getCroppedCanvas().toDataURL())
   }
 
   /* 编码(唯一) */
@@ -120,12 +138,36 @@ const JGImageCutter = function (props: JGImageCutterProps) {
 
   return (
     <Box style={wrapStyles}>
-      <img src={logo} />
+      <Cropper
+        dragMode="crop"
+        background={false}
+        viewMode={1}
+        autoCrop={false}
+        src={logo}
+        cropend={myCrop}
+        preview=".myTest"
+        ref={cropperRef}
+        zoomable={false}
+        zoomOnTouch={true}
+        zoomOnWheel={true}
+        style={{
+          height: '300px',
+          width: '300px'
+        }}
+      ></Cropper>
     </Box>
   )
 }
 
-JGImageCutter.defaultProps = {}
+JGImageCutter.defaultProps = {
+  left: 0,
+  top: 0,
+  height: '200px',
+  width: '200px',
+  multiWidth: '200px',
+  labelWidth: '200px',
+  visible: true
+}
 
 export default JGImageCutter
-export { JGImageCutter }
+export { JGImageCutter, type JGImageCutterProps }
