3794089e1d9e99e0f7b086be86d22971cf959e90
三分归元气 <1801520013@qq.com>
2021-12-30 20:13:23 +0800

    表格样式修改

--- packages/widgets/JGDataGrid/src/JGDataGrid.tsx
+++ packages/widgets/JGDataGrid/src/JGDataGrid.tsx
@@ -17,20 +17,19 @@ interface dataHeader {
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
     backgroundColor: 'rgb(246, 247, 251)',
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+
+    textAlign: 'center'
   },
   [`&.${tableCellClasses.body}`]: {
-    fontSize: 14
+    fontSize: 14,
+    borderRight: '1px solid #DCDEE2'
   }
 }))
 
 const StyledTableRow = styled(TableRow)(({ theme }) => ({
   '&:nth-of-type(odd)': {
-    backgroundColor: theme.palette.action.hover
-  },
-  // hide last border
-  '&:last-child td, &:last-child th': {
-    border: 0
+    backgroundColor: '#FBFBFB'
   }
 }))
 
@@ -67,22 +66,24 @@ const JGDataGrid = (props: JGDataGridProps) => {
         left: left,
         top: top,
         width: '100%',
-        height: toHeight(height, context, '300px'),
+        height: '100%',
         position: context.position,
         display: ishide ? 'none' : 'block',
-        border: '1px solid #DCDEE2',
+        // border: '1px solid #DCDEE2',
         boxShadow: 'none'
       }}
       component={Paper}
     >
-      <Table sx={{ minWidth: 650 }} size="small" {...resprops}>
+      <Table sx={{ width: '100%', height: '100%' }} size="small" {...resprops}>
         <TableHead>
-          <TableRow
-            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            key="80000"
-          >
+          <TableRow key="80000">
             {dataHeader?.map((row) => (
-              <StyledTableCell component="th" scope="row" key={row.code}>
+              <StyledTableCell
+                sx={{ border: '1px solid #DCDEE2' }}
+                component="th"
+                scope="row"
+                key={row.code}
+              >
                 {row.name}
               </StyledTableCell>
             ))}
@@ -90,16 +91,17 @@ const JGDataGrid = (props: JGDataGridProps) => {
         </TableHead>
         <TableBody>
           {data.map((row: any) => (
-            <TableRow
-              key={row.id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
+            <StyledTableRow key={row.id}>
               {dataHeader?.map((item) => (
-                <TableCell scope="row" key={item.code}>
+                <TableCell
+                  sx={{ border: '1px solid #DCDEE2!important' }}
+                  scope="row"
+                  key={item.code}
+                >
                   {row[item.code]}
                 </TableCell>
               ))}
-            </TableRow>
+            </StyledTableRow>
           ))}
         </TableBody>
       </Table>
@@ -111,18 +113,28 @@ JGDataGrid.defaultProps = {
   left: '20px',
   top: '50px',
   width: '800px',
-  height: '500px',
+  height: 'auto',
   position: 'absolute',
   data: [
-    { id: '1', name: '11', num: '120', key: '1' },
-    { id: '2', name: '22', num: '122', key: '2' },
-    { id: '3', name: '33', num: '130', key: '3' },
-    { id: '4', name: '44', num: '140', key: '4' }
+    { index: '1', name: '', num: '', key: '1' },
+    { index: '2', name: '', num: '', key: '2' },
+    { index: '3', name: '', num: '', key: '3' },
+    { index: '4', name: '', num: '', key: '4' },
+    { index: '5', name: '', num: '', key: '4' },
+    { index: '6', name: '', num: '', key: '4' },
+    { index: '7', name: '', num: '', key: '4' }
   ],
   dataHeader: [
-    { code: 'id', name: 'id号', key: '5' },
-    { code: 'name', name: '名称', key: '6' },
-    { code: 'num', name: '数量', key: '7' }
+    { code: 'index', name: '', key: '8' },
+    { code: 'id', name: '文本', key: '5' },
+    { code: 'name', name: '文本', key: '6' },
+    { code: 'num', name: '文本', key: '7' },
+    { code: 'text1', name: '文本', key: '5' },
+    { code: 'text2', name: '小数', key: '6' },
+    { code: 'text3', name: '下拉选择', key: '7' },
+    { code: 'text4', name: '日期', key: '5' },
+    { code: 'text5', name: '弹出选择', key: '6' },
+    { code: 'text6', name: '图片', key: '7' }
   ]
 }
 
