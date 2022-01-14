3cc890a0e2dfe0cab488399eedc8442fc91793df
zhougege520 <2912354876@qq.com>
2022-01-14 11:03:41 +0800

    多选框，单选框绑定数据

--- packages/widgets/JGRadioGroup/src/index.tsx
+++ packages/widgets/JGRadioGroup/src/index.tsx
@@ -1,5 +1,9 @@
 import { Control } from '@v-act/schema-types'
 import {
+  getColumnName,
+  getDropDownSource,
+  getIDColumnName,
+  getTableName,
   toBoolean,
   toNumber,
   valueofHeight,
@@ -12,17 +16,25 @@ const convert = function (
   control: Control,
   render: (controls: Array<Control>) => JSX.Element | null
 ): JSX.Element {
+  console.log('@@@@@@@@@@@@@@@:JGRadioGroup')
+  console.log(control)
   const pros = control.properties
   const props: JGRadioGroupProps = {
     top: toNumber(pros.top) + 'px',
     left: toNumber(pros.left) + 'px',
     labelText: pros.labelText,
-    placeholder: pros.placeholder,
     visible: toBoolean(pros.visible, true),
+    isMust: toBoolean(pros.isMust, false),
+    readOnly: toBoolean(pros.readOnly, false),
+    enabled: toBoolean(pros.enabled, true),
     labelWidth: toNumber(pros.labelWidth),
     labelVisible: toBoolean(pros.labelVisible, true),
     multiWidth: valueofWidth(pros.multiWidth, '235px'),
-    multiHeight: valueofHeight(pros.multiHeight, '26px')
+    multiHeight: valueofHeight(pros.multiHeight, '26px'),
+    tableName: getTableName(control),
+    columnName: getColumnName(control),
+    idColumnName: getIDColumnName(control),
+    dropDownSource: getDropDownSource(control)
   }
   return <JGRadioGroup {...props}></JGRadioGroup>
 }
