03892b5f194dfeaa2d7778992805efcb74712cc7
hwk394020931 <394020931@qq.com>
2022-01-19 11:54:33 +0800

    轮播图临时提交

--- packages/widgets/JGImagePlay/src/JGImagePlay.tsx
+++ packages/widgets/JGImagePlay/src/JGImagePlay.tsx
@@ -7,19 +7,35 @@ import { autoPlay } from 'react-swipeable-views-utils'
 import { KeyboardArrowLeft, KeyboardArrowRight } from '@mui/icons-material'
 import { Box, BoxProps, Button, MobileStepper } from '@mui/material'
 import { Height, Width } from '@v-act/schema-types'
-import { useContext } from '@v-act/widget-context'
-import { toHeight, toWidth } from '@v-act/widget-utils'
+import { EntityRecord, useContext } from '@v-act/widget-context'
+import {
+  getComponentResPath,
+  getEntityDatas,
+  toHeight,
+  toWidth
+} from '@v-act/widget-utils'
 
 const AutoPlaySwipeableViews = autoPlay(SwipeableViews)
 interface JGImagePlayProps extends BoxProps {
+  /** 其他 */
   /**
-   * 左边距
+   * 控件名称
    */
-  left?: Property.Left
+  alias?: string
+
+  /** 格式 */
+  /**
+   * 图片布局方式
+   */
+  imageLayout?: string
   /**
    * 上边距
    */
   top?: Property.Top
+  /**
+   * 左边距
+   */
+  left?: Property.Left
   /**
    * 高度
    */
@@ -28,37 +44,66 @@ interface JGImagePlayProps extends BoxProps {
    * 宽度
    */
   multiWidth?: Width
-}
+  /**
+   * 播放间隔(秒)
+   */
+  swiInterval?: number
+  /**
+   * 显示页码
+   */
+  paginationVisible?: boolean
 
-const images = [
-  {
-    imgPath:
-      // 'http://www.yindangu.com/itop/common/images/defaultImg.png'
-      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60'
-  },
-  {
-    imgPath:
-      // 'http://www.yindangu.com/itop/common/images/defaultImg.png'
-      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60'
-  },
-  {
-    imgPath:
-      // 'http://www.yindangu.com/itop/common/images/defaultImg.png'
-      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80'
-  },
-  {
-    imgPath:
-      // 'http://www.yindangu.com/itop/common/images/defaultImg.png'
-      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60'
-  }
-]
+  /** 数据 */
+  /**
+   * 图片标识字段
+   */
+  imageField?: string
+  /**
+   * 图片名称字段
+   */
+  imageNameField?: string
+  /**
+   * 实体
+   */
+  tableName?: string | null
+  /**
+   * 构件Code
+   */
+  componentCode?: string
+
+  /**
+   * 点击事件
+   */
+  click?: Function
+}
 
 const JGImagePlay = function (props: JGImagePlayProps) {
   const context = useContext()
+  console.log(context, 'JGImagePlay======context')
+  //图片url生成
+  let images: EntityRecord[] = []
+  if (props.tableName) {
+    images = getEntityDatas(props.tableName, context) || []
+  }
+
+  images.forEach(function (item) {
+    ;(item.imgPath =
+      item.ImageField && (props.componentCode ? props.componentCode : '')
+        ? getComponentResPath(
+            item.ImageField as string,
+            props.componentCode ? props.componentCode : ''
+          )
+        : 'http://vstore-proto.yindangu.com/itop/resources/e425207964b340019291a8a2f25c7237_' +
+          item.ImageField),
+      (item.imgLabel = item.ImageDescField)
+  })
+
   const width = toWidth(props.multiWidth, context, '235px')
   const height = toHeight(props.multiHeight, context, '26px')
-
   const [activeStep, setActiveStep] = React.useState(0)
+
+  //是否显示页码
+  // const maxStepsVisible = props.paginationVisible ? images.length : 0;
   const maxSteps = images.length
 
   const handleNext = () => {
@@ -83,60 +128,122 @@ const JGImagePlay = function (props: JGImagePlayProps) {
     top: props.top
   }
 
+  const labelStyle: CSSProperties = {
+    width: '100%',
+    height: '40px',
+    backgroundColor: 'rgb(0,0,0,0.3)',
+    position: 'absolute',
+    bottom: '1px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    color: 'white',
+    lineHeight: '40px'
+  }
+
+  const labelLeft: CSSProperties = {
+    marginLeft: '10px'
+  }
+
+  const labelRight: CSSProperties = {
+    marginRight: '10px'
+  }
+
+  const stepper: CSSProperties = {
+    width: '100%',
+    background: 'none',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    padding: 0
+  }
+
+  const buttonStyle: CSSProperties = {
+    background: 'black',
+    opacity: '0.5',
+    color: 'white',
+    minHeight: '60px',
+    padding: 0,
+    minWidth: '38px'
+  }
+
+  const handleClick = function () {
+    props.click && props.click()
+  }
+
+  //轮播时间处理
+  const times: number = (props.swiInterval ? props.swiInterval : 5) * 1000
+  console.log(props, 'sssssssssssssssssssss')
+
   return (
     <Box style={wrapStyles}>
-      <AutoPlaySwipeableViews
-        index={activeStep}
-        onChangeIndex={handleStepChange}
-        enableMouseEvents
-      >
-        {images.map((step, index) => (
-          <div>
-            {Math.abs(activeStep - index) <= 2 ? (
-              <Box
-                component="img"
-                sx={{
-                  height: '120px',
-                  display: 'block',
-                  maxWidth: 400,
-                  overflow: 'hidden',
-                  width: '100%'
-                }}
-                src={step.imgPath}
-              />
-            ) : null}
-          </div>
-        ))}
-      </AutoPlaySwipeableViews>
-      <MobileStepper
-        steps={maxSteps}
-        position="static"
-        activeStep={activeStep}
-        nextButton={
-          <Button
-            size="small"
-            onClick={handleNext}
-            disabled={activeStep === maxSteps - 1}
-          >
-            Next
-            {/* {theme.direction === 'rtl' ? ( */}
-            <KeyboardArrowLeft />
-            {/* // ) : (
-            //   <KeyboardArrowRight />
-            // )} */}
-          </Button>
-        }
-        backButton={
-          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-            {/* {theme.direction === 'rtl' ? ( */}
-            <KeyboardArrowRight />
-            {/* ) : (
+      <div style={{ position: 'relative' }}>
+        <AutoPlaySwipeableViews
+          index={activeStep}
+          onChangeIndex={handleStepChange}
+          interval={times}
+          enableMouseEvents
+          onClick={handleClick}
+        >
+          {images.map((step, index) => (
+            <div style={{ position: 'relative' }}>
+              {Math.abs(activeStep - index) <= 2 ? (
+                <Box
+                  component="img"
+                  sx={{
+                    height: '100%',
+                    display: 'block',
+                    maxWidth: width,
+                    maxHeight: height,
+                    overflow: 'hidden',
+                    margin: '0 auto',
+                    width: props.imageLayout == 'Stretch' ? '100%' : 'auto'
+                  }}
+                  src={step.imgPath as string}
+                />
+              ) : null}
+            </div>
+          ))}
+        </AutoPlaySwipeableViews>
+        <div style={labelStyle}>
+          <span style={labelLeft}>{images[activeStep].imgLabel}</span>
+          <span style={labelRight}>
+            {activeStep + 1}/{maxSteps}
+          </span>
+        </div>
+        <MobileStepper
+          style={stepper}
+          steps={0}
+          position="static"
+          activeStep={activeStep}
+          nextButton={
+            // <div style={buttonStyle}>
+            <Button
+              style={buttonStyle}
+              sx={{
+                borderRadius: '0 4px 4px 0'
+              }}
+              size="small"
+              onClick={handleNext}
+              disabled={activeStep === maxSteps - 1}
+            >
               <KeyboardArrowLeft />
-            )} */}
-            Back
-          </Button>
-        }
-      />
+            </Button>
+          }
+          backButton={
+            <Button
+              style={buttonStyle}
+              sx={{
+                borderRadius: '4px 0 0 4px'
+              }}
+              size="small"
+              onClick={handleBack}
+              disabled={activeStep === 0}
+            >
+              <KeyboardArrowRight />
+            </Button>
+          }
+        />
+      </div>
     </Box>
   )
 }
@@ -145,7 +252,9 @@ JGImagePlay.defaultProps = {
   left: '0px',
   top: '0px',
   multiHeight: '170px',
-  multiWidth: '263px'
+  multiWidth: '263px',
+  swiInterval: 5,
+  paginationVisible: true
 }
 
 export default JGImagePlay
