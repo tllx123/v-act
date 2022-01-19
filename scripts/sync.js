8be11e47d6845891538c6bc04d3d27e50deaa743
Chavy <chavyleung@gmail.com>
2022-01-19 09:39:23 +0800

    chore: add sync: widgets

---
+++ scripts/sync.js
@@ -0,0 +1,23 @@
+import fs from 'fs-extra'
+import path from 'path'
+
+import { filterPackages } from '@lerna/filter-packages'
+import { getPackagesSync } from '@lerna/project'
+
+export function sync(scopes, copyToPath) {
+  const packages = getPackagesSync()
+  const filteredPackages = filterPackages(packages, scopes)
+  filteredPackages.forEach((pkg) => {
+    const distDir = path.resolve(`${copyToPath}/node_modules/${pkg.name}/`)
+    fs.rmSync(distDir, {
+      force: true,
+      recursive: true
+    })
+    fs.copySync(pkg.location, distDir)
+    const nodemodules = path.resolve(`${distDir}/node_modules/`)
+    fs.rmSync(nodemodules, {
+      recursive: true,
+      force: true
+    })
+  })
+}
