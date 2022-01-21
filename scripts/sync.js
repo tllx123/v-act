d2a94120afca692a8139ff2e43ac4bb2c1787736
Chavy <chavyleung@gmail.com>
2022-01-21 11:01:38 +0800

    fixed(sync.js): sync error

--- scripts/sync.js
+++ scripts/sync.js
@@ -6,14 +6,17 @@ import { getPackagesSync } from '@lerna/project'
 
 export function sync(scopes, copyToPath) {
   const packages = getPackagesSync()
-  const filteredPackages = filterPackages(packages, [
-    '@v-act/schema-types',
-    '@v-act/widget-context',
-    '@v-act/widget-utils',
-    '@v-act/component-schema-utils',
-    '@v-act/window-schema-utils',
-    scopes
-  ])
+  const filteredPackages = filterPackages(
+    packages,
+    [
+      '@v-act/schema-types',
+      '@v-act/widget-context',
+      '@v-act/widget-utils',
+      '@v-act/component-schema-utils',
+      '@v-act/window-schema-utils',
+      scopes
+    ].flat()
+  )
   filteredPackages.forEach((pkg) => {
     const distDir = path.resolve(`${copyToPath}/node_modules/${pkg.name}/`)
     fs.rmSync(distDir, {
