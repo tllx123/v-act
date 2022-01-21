47bcd3475b8ef8870702d171aea2f326132cd777
三分归元气 <1801520013@qq.com>
2022-01-21 09:22:16 +0800

    富文本

--- packages/widgets/JGRichTextEditor/src/JGRichTextEditor.tsx
+++ packages/widgets/JGRichTextEditor/src/JGRichTextEditor.tsx
@@ -1,30 +1,11 @@
-import React, { useCallback, useMemo, useState } from 'react'
-import isHotkey from 'is-hotkey'
+import React, { useState } from 'react'
 import { useContext } from '@v-act/widget-context'
 import { Property } from 'csstype'
-import { Editable, withReact, useSlate, Slate } from 'slate-react'
-import { toHeight, toLabelWidth, toWidth } from '@v-act/widget-utils'
+import { toHeight, toWidth } from '@v-act/widget-utils'
 import Box from '@mui/material/Box'
-import '../src/icon.css'
-import {
-  Editor,
-  Transforms,
-  createEditor,
-  Descendant,
-  Element as SlateElement
-} from 'slate'
-import { withHistory } from 'slate-history'
-import { Button, Toolbar } from '../src/components'
-import Icon from '@mui/material/Icon'
-
-const HOTKEYS = {
-  'mod+b': 'bold',
-  'mod+i': 'italic',
-  'mod+u': 'underline',
-  'mod+`': 'code'
-}
-
-const LIST_TYPES = ['numbered-list', 'bulleted-list']
+import ReactQuill from 'react-quill'
+import 'react-quill/dist/quill.snow.css'
+import TextareaAutosize from '@mui/material/TextareaAutosize'
 
 export interface JGRichTextEditorProps {
   left?: Property.Left
@@ -32,196 +13,52 @@ export interface JGRichTextEditorProps {
   position?: Property.Position
   height?: Property.Height
   width?: Property.Width
+  tablename?: string | null
+  columnname?: string | null
+  control?: any
 }
 
 const JGRichTextEditor = (props: JGRichTextEditorProps) => {
   const context = useContext()
   const { left, top, height, width, position, ...resprops } = props
 
-  const [value, setValue] = useState<Descendant[]>(initialValue)
-  const renderElement = useCallback((props) => <Element {...props} />, [])
-  const renderLeaf = useCallback((props) => <Leaf {...props} />, [])
-  const editor = useMemo(() => withHistory(withReact(createEditor())), [])
-
+  const [value, setValue] = useState('')
   return (
     <Box
       sx={{
-        width: toWidth(width, context, '235px'),
-        height: toHeight(height, context, '26px'),
-        position: context.position,
-        left: left,
-        top: top
+        'width': toWidth(width, context, '235px'),
+        'height': toHeight(height, context, '26px'),
+        'position': context.position,
+        'left': left,
+        'top': top,
+        'resize': 'none',
+        'border': '1px solid #dcdee2',
+        'color': '#333',
+        'borderRadius': '4px',
+        '&:hover': {
+          borderColor: '#356abb'
+        },
+        '&:focus': {
+          borderColor: '#356abb',
+          background: '#fff',
+          boxShadow: '0 0 0 2px rgba(53, 106, 187, 0.3)',
+          outline: 'none'
+        },
+        '&:disabled': {
+          background: '#f6f7fb'
+        }
       }}
+      component={TextareaAutosize}
     >
-      <Slate
-        editor={editor}
-        value={value}
-        onChange={(value) => setValue(value)}
+      {/* <Box
+        sx={{ width: '100%', height: "100%" }}
+        component={ReactQuill}
+        theme="snow" value={value} onChange={setValue}
       >
-        <Toolbar>
-          <MarkButton format="bold" icon="format_bold" />
-          <MarkButton format="italic" icon="format_italic" />
-          <MarkButton format="underline" icon="format_underlined" />
-          <MarkButton format="code" icon="code" />
-          <BlockButton format="heading-one" icon="looks_one" />
-          <BlockButton format="heading-two" icon="looks_two" />
-          <BlockButton format="block-quote" icon="format_quote" />
-          <BlockButton format="numbered-list" icon="format_list_numbered" />
-          <BlockButton format="bulleted-list" icon="format_list_bulleted" />
-        </Toolbar>
-        <Editable
-          renderElement={renderElement}
-          renderLeaf={renderLeaf}
-          placeholder="输入内容"
-          spellCheck
-          autoFocus
-          onKeyDown={(event) => {
-            for (const hotkey in HOTKEYS) {
-              if (isHotkey(hotkey, event as any)) {
-                event.preventDefault()
-                const mark = HOTKEYS[hotkey]
-                toggleMark(editor, mark)
-              }
-            }
-          }}
-        />
-      </Slate>
+      </Box> */}
     </Box>
   )
 }
-
-const toggleBlock = (editor: any, format: any) => {
-  const isActive = isBlockActive(editor, format)
-  const isList = LIST_TYPES.includes(format)
-
-  Transforms.unwrapNodes(editor, {
-    match: (n) =>
-      !Editor.isEditor(n) &&
-      SlateElement.isElement(n) &&
-      LIST_TYPES.includes(n.type),
-    split: true
-  })
-  const newProperties: Partial<SlateElement> = {
-    type: isActive ? 'paragraph' : isList ? 'list-item' : format
-  }
-  Transforms.setNodes<SlateElement>(editor, newProperties)
-
-  if (!isActive && isList) {
-    const block = { type: format, children: [] }
-    Transforms.wrapNodes(editor, block)
-  }
-}
-
-const toggleMark = (editor: any, format: any) => {
-  const isActive = isMarkActive(editor, format)
-
-  if (isActive) {
-    Editor.removeMark(editor, format)
-  } else {
-    Editor.addMark(editor, format, true)
-  }
-}
-
-const isBlockActive = (editor: any, format: any) => {
-  const { selection } = editor
-  if (!selection) return false
-
-  const [match] = Array.from(
-    Editor.nodes(editor, {
-      at: Editor.unhangRange(editor, selection),
-      match: (n) =>
-        !Editor.isEditor(n) && SlateElement.isElement(n) && n.type === format
-    })
-  )
-
-  return !!match
-}
-
-const isMarkActive = (editor: any, format: any) => {
-  const marks = Editor.marks(editor)
-  return marks ? marks[format] === true : false
-}
-
-const Element = ({ attributes, children, element }: any) => {
-  switch (element.type) {
-    case 'block-quote':
-      return <blockquote {...attributes}>{children}</blockquote>
-    case 'bulleted-list':
-      return <ul {...attributes}>{children}</ul>
-    case 'heading-one':
-      return <h1 {...attributes}>{children}</h1>
-    case 'heading-two':
-      return <h2 {...attributes}>{children}</h2>
-    case 'list-item':
-      return <li {...attributes}>{children}</li>
-    case 'numbered-list':
-      return <ol {...attributes}>{children}</ol>
-    default:
-      return <p {...attributes}>{children}</p>
-  }
-}
-
-const Leaf = ({ attributes, children, leaf }: any) => {
-  if (leaf.bold) {
-    children = <strong>{children}</strong>
-  }
-
-  if (leaf.code) {
-    children = <code>{children}</code>
-  }
-
-  if (leaf.italic) {
-    children = <em>{children}</em>
-  }
-
-  if (leaf.underline) {
-    children = <u>{children}</u>
-  }
-
-  return <span {...attributes}>{children}</span>
-}
-
-interface params {
-  format: string
-  icon: string
-}
-
-const BlockButton = ({ format, icon }: params) => {
-  const editor = useSlate()
-  return (
-    <Button
-      active={isBlockActive(editor, format)}
-      onMouseDown={(event: any) => {
-        event.preventDefault()
-        toggleBlock(editor, format)
-      }}
-    >
-      <Icon>{icon}</Icon>
-    </Button>
-  )
-}
-
-const MarkButton = ({ format, icon }: params) => {
-  const editor = useSlate()
-  return (
-    <Button
-      active={isMarkActive(editor, format)}
-      onMouseDown={(event: any) => {
-        event.preventDefault()
-        toggleMark(editor, format)
-      }}
-    >
-      <Icon>{icon}</Icon>
-    </Button>
-  )
-}
-
-const initialValue: Descendant[] = [
-  {
-    type: 'paragraph',
-    children: [{ text: '' }]
-  }
-]
 
 JGRichTextEditor.defaultProps = {
   left: '20px',
