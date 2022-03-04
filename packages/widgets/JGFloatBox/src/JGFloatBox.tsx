77dbb207139150ca2728cdd5be70daf7e34fe307
yzc <123@123.com>
2022-03-04 15:26:26 +0800

    新增（小数组件 - 值同步更改）

--- packages/widgets/JGFloatBox/src/JGFloatBox.tsx
+++ packages/widgets/JGFloatBox/src/JGFloatBox.tsx
@@ -1,90 +1,10 @@
-import React, { CSSProperties, forwardRef, useState } from 'react'
-
-import { Property } from 'csstype'
+import React, { forwardRef } from 'react'
 
 import InputUnstyled, { InputUnstyledProps } from '@mui/base/InputUnstyled'
+import Box from '@mui/material/Box'
 import { styled } from '@mui/system'
 import { JGInputLabel } from '@v-act/jginputlabel'
-import { Height, Width } from '@v-act/schema-types'
-import { FieldValue, useContext } from '@v-act/widget-context'
-import {
-  getFieldValue,
-  isNullOrUnDef,
-  toHeight,
-  toLabelWidth,
-  toWidth
-} from '@v-act/widget-utils'
-
-interface JGFloatBoxProps extends InputUnstyledProps {
-  /**
-   * 左边距
-   */
-  left?: Property.Left
-  /**
-   * 上边距
-   */
-  top?: Property.Top
-  /**
-   * 高度
-   */
-  multiHeight?: Height
-  /**
-   * 宽度
-   */
-  multiWidth?: Width
-  /**
-   * 标题
-   */
-  labelText?: string
-  /**
-   * 标题宽度
-   */
-  labelWidth?: number
-  /**
-   * 提醒文字
-   */
-  placeholder?: string
-  /**
-   * 必填
-   */
-  isMust?: boolean
-  /**
-   * 显示
-   */
-  visible?: boolean
-
-  /**
-   * 显示标题
-   */
-  labelVisible?: boolean
-
-  /**
-   * 显示宽度
-   */
-  width?: Property.Width
-
-  /**
-   * 显示高度
-   */
-  height?: Property.Height
-
-  /**
-   * 输入框显示类型
-   */
-  inputType?: string
-  /**
-   * 禁用
-   */
-  disabled?: boolean
-  /**
-   * 实体编号
-   */
-  tableName?: string | null
-  /**
-   * 字段编号
-   */
-  columnName?: string | null
-}
+import { JGComponentProps } from '@v-act/schema-types'
 
 const StyledInputElement = styled('input')`
   border: 1px solid #dcdee2;
@@ -115,7 +35,7 @@ const StyledInputElement = styled('input')`
 `
 
 const CustomInput = forwardRef(function (
-  props: JGFloatBoxProps,
+  props: InputUnstyledProps,
   ref: React.ForwardedRef<HTMLDivElement>
 ) {
   return (
@@ -127,92 +47,81 @@ const CustomInput = forwardRef(function (
   )
 })
 
-const JGFloatBox = function (props: JGFloatBoxProps) {
-  if (!props.visible) {
-    return null
-  }
-  const context = useContext()
-  let value: FieldValue = ''
-  if (props.tableName && props.columnName) {
-    value = getFieldValue(props.tableName, props.columnName, context)
-    value = isNullOrUnDef(value) ? '' : value
-  }
-  const [inputVal, setInputVal] = useState(value)
-  const width = toWidth(props.multiWidth, context, '235px')
-  const height = toHeight(props.multiHeight, context, '26px')
-  const labelWidth = props.labelVisible
-    ? toLabelWidth(props.labelWidth, context, 94)
-    : 0
-  const wrapStyles: CSSProperties = {
-    width: width,
-    height: height,
-    fontSize: '14px',
-    position: context.position,
-    display: 'flex',
-    alignItems: 'center',
-    left: props.left,
-    top: props.top,
-    fontFamily:
-      'Helvetica Neue,Helvetica,PingFang SC,Hiragino Sans GB,Microsoft YaHei,\\5FAE\\8F6F\\96C5\\9ED1,Arial,sans-serif'
+const JGFloatBox = function (props: JGComponentProps) {
+  const {
+    left,
+    top,
+    height,
+    width,
+    ismust,
+    labeltext,
+    margin,
+    padding,
+    readonly,
+    labelVisible,
+    labelWidth,
+    disabled,
+    position,
+    value,
+    onChanged,
+    placeholder
+  } = props
+
+  let isDisable = false
+  if (readonly || disabled) {
+    isDisable = true
   }
   const inputStyles = {
     width: '100%',
     height: height
   }
-  const isInteger = (e) => {
-    if (props.inputType === 'integer') {
-      let filterVal = e.target.value.replace(/[^(-?\d)]/gi, '')
-      if (
-        filterVal.lastIndexOf('-') !== -1 &&
-        filterVal.lastIndexOf('-') !== 0
-      ) {
-        filterVal = '-' + filterVal.replace(/[^\d]/gi, '')
-      }
-      setInputVal(filterVal)
-    } else {
-      setInputVal(e.target.value)
-    }
-  }
-  const handleChange = (e) => {
-    isInteger(e)
-  }
   return (
-    <div style={wrapStyles} value-show={value}>
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        width: width,
+        height: height,
+        position: position,
+        left: left,
+        top: top,
+        margin: margin,
+        padding: padding,
+        pointerEvents: readonly ? 'none' : 'auto'
+      }}
+    >
       <JGInputLabel
         width={labelWidth}
         height={height}
-        visible={props.labelVisible}
-        required={props.isMust}
+        visible={labelVisible}
+        required={ismust}
       >
-        {props.labelText}
+        {labeltext}
       </JGInputLabel>
       <CustomInput
         style={inputStyles}
-        placeholder={props.placeholder}
-        type={props.inputType === 'integer' ? 'text' : props.inputType}
-        onChange={(e) => handleChange(e)}
-        value={inputVal}
-        disabled={props.disabled}
+        placeholder={placeholder}
+        type={'number'}
+        onChange={(e) => {
+          onChanged && onChanged(e)
+        }}
+        disabled={isDisable}
+        value={value}
       />
-    </div>
+    </Box>
   )
 }
 
 JGFloatBox.defaultProps = {
-  left: '0px',
-  top: '0px',
-  multiHeight: '26px',
-  multiWidth: '235px',
-  labelWidth: 94,
-  labelText: '文本',
+  labeltext: '文本',
+  width: '235px',
+  height: '26px',
+  position: 'absolute',
+  left: 0,
+  top: 0,
   placeholder: '',
-  isMust: false,
-  visible: true,
   labelVisible: true,
-  inputType: 'number',
-  disabled: false
+  labelWidth: 94
 }
 
 export default JGFloatBox
-export { JGFloatBox }
-export type { JGFloatBoxProps }
--- packages/widgets/JGFloatBox/src/index.tsx
+++ packages/widgets/JGFloatBox/src/index.tsx
@@ -1,53 +1,11 @@
-import {
-  Control,
-  WidgetRenderContext,
-  WidgetRenderer
-} from '@v-act/schema-types'
-import {
-  getColumnName,
-  getTableName,
-  toBoolean,
-  toNumber,
-  valueofHeight,
-  valueofWidth
-} from '@v-act/widget-utils'
+import { Control } from '@v-act/schema-types'
+import { useGetCompVal } from '@v-act/widget-utils'
 
-import { JGFloatBox, JGFloatBoxProps } from './JGFloatBox'
+import JGFloatBox from './JGFloatBox'
 
-const JsonJGFloatBox = function (props: {
-  control: Control
-  render: WidgetRenderer
-  componentCode: string
-  context: WidgetRenderContext
-}) {
-  return convert(props.control, props.render)
+const JsonJGFloatBox = function (props: { control: Control }): JSX.Element {
+  let propsTemp: Object = useGetCompVal(props.control)
+  return <JGFloatBox {...propsTemp}></JGFloatBox>
 }
 
-const convert = function (
-  control: Control,
-  render: WidgetRenderer
-): JSX.Element {
-  const pros = control.properties
-  const props: JGFloatBoxProps = {
-    top: toNumber(pros.top) + 'px',
-    left: toNumber(pros.left) + 'px',
-    width: valueofWidth(pros.multiWidth, '235px'),
-    height: valueofHeight(pros.multiHeight, '26px'),
-    labelText: pros.labelText || '文本',
-    isMust: toBoolean(pros.isMust),
-    placeholder: pros.placeholder,
-    visible: toBoolean(pros.visible, true),
-    labelWidth: toNumber(pros.labelWidth),
-    labelVisible: toBoolean(pros.labelVisible, true),
-    multiWidth: valueofWidth(pros.multiWidth, '235px'),
-    multiHeight: valueofHeight(pros.multiHeight, '26px'),
-    disabled: !toBoolean(pros.enabled, true),
-    tableName: getTableName(control),
-    columnName: getColumnName(control)
-  }
-  return <JGFloatBox {...props}></JGFloatBox>
-}
-
-export default JGFloatBox
-
-export { convert, JGFloatBox, JsonJGFloatBox }
+export { JGFloatBox, JsonJGFloatBox }
