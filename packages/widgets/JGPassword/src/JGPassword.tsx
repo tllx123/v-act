d17f2fa005705c72f7719a173850dca1f9310bbf
yzc <123@123.com>
2022-03-04 16:20:18 +0800

    新增（密码组件 - 值同步更改）

--- packages/widgets/JGPassword/src/JGPassword.tsx
+++ packages/widgets/JGPassword/src/JGPassword.tsx
@@ -1,90 +1,10 @@
-import React, { CSSProperties, forwardRef, useState } from 'react'
-
-import { Property } from 'csstype'
+import React, { forwardRef } from 'react'
 
 import InputUnstyled, { InputUnstyledProps } from '@mui/base/InputUnstyled'
+import Box from '@mui/material/Box'
 import { styled } from '@mui/system'
 import { JGInputLabel } from '@v-act/jginputlabel'
-import { Height, Width } from '@v-act/schema-types'
-import { FieldValue, useContext } from '@v-act/widget-context'
-import {
-  getFieldValue,
-  isNullOrUnDef,
-  toHeight,
-  toLabelWidth,
-  toWidth
-} from '@v-act/widget-utils'
-
-interface JGPasswordProps extends InputUnstyledProps {
-  /**
-   * 左边距
-   */
-  left?: Property.Left
-  /**
-   * 上边距
-   */
-  top?: Property.Top
-  /**
-   * 高度
-   */
-  multiHeight?: Height
-  /**
-   * 宽度
-   */
-  multiWidth?: Width
-  /**
-   * 标题
-   */
-  labelText?: string
-  /**
-   * 标题宽度
-   */
-  labelWidth?: number
-  /**
-   * 提醒文字
-   */
-  placeholder?: string
-  /**
-   * 必填
-   */
-  isMust?: boolean
-  /**
-   * 显示
-   */
-  visible?: boolean
-
-  /**
-   * 显示标题
-   */
-  labelVisible?: boolean
-
-  /**
-   * 显示宽度
-   */
-  width?: Property.Width
-
-  /**
-   * 显示高度
-   */
-  height?: Property.Height
-
-  /**
-   * 输入框显示类型
-   */
-  inputType?: string
-  /**
-   * 禁用
-   */
-  disabled?: boolean
-  /**
-   * 实体编号
-   */
-  tableName?: string | null
-  /**
-   * 字段编号
-   */
-  columnName?: string | null
-}
+import { JGComponentProps } from '@v-act/schema-types'
 
 const StyledInputElement = styled('input')`
   border: 1px solid #dcdee2;
@@ -115,7 +35,7 @@ const StyledInputElement = styled('input')`
 `
 
 const CustomInput = forwardRef(function (
-  props: JGPasswordProps,
+  props: InputUnstyledProps,
   ref: React.ForwardedRef<HTMLDivElement>
 ) {
   return (
@@ -127,75 +47,72 @@ const CustomInput = forwardRef(function (
   )
 })
 
-const JGPassword = function (props: JGPasswordProps) {
-  if (!props.visible) {
-    return null
-  }
+const JGPassword = function (props: JGComponentProps) {
+  const {
+    left,
+    top,
+    height,
+    width,
+    ismust,
+    labeltext,
+    margin,
+    padding,
+    readonly,
+    labelVisible,
+    labelWidth,
+    disabled,
+    position,
+    value,
+    onChanged,
+    placeholder
+  } = props
 
-  const context = useContext()
-  let value: FieldValue = ''
-  if (props.tableName && props.columnName) {
-    value = getFieldValue(props.tableName, props.columnName, context)
-    value = isNullOrUnDef(value) ? '' : value
-  }
-  const [inputVal, setInputVal] = useState(value)
-  const width = toWidth(props.multiWidth, context, '235px')
-  const height = toHeight(props.multiHeight, context, '26px')
-  const labelWidth = props.labelVisible
-    ? toLabelWidth(props.labelWidth, context, 94)
-    : 0
-  const wrapStyles: CSSProperties = {
-    width: width,
-    height: height,
-    fontSize: '14px',
-    position: context.position,
-    display: 'flex',
-    alignItems: 'center',
-    left: props.left,
-    top: props.top,
-    fontFamily:
-      'Helvetica Neue,Helvetica,PingFang SC,Hiragino Sans GB,Microsoft YaHei,\\5FAE\\8F6F\\96C5\\9ED1,Arial,sans-serif'
-  }
-  const inputStyles = {
-    width: '100%',
-    height: height
-  }
   return (
-    <div style={wrapStyles}>
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        width: width,
+        height: height,
+        position: position,
+        left: left,
+        top: top,
+        margin: margin,
+        padding: padding,
+        pointerEvents: readonly ? 'none' : 'auto'
+      }}
+    >
       <JGInputLabel
         width={labelWidth}
         height={height}
-        visible={props.labelVisible}
-        required={props.isMust}
+        visible={labelVisible}
+        required={ismust}
       >
-        {props.labelText}
+        {labeltext}
       </JGInputLabel>
       <CustomInput
-        style={inputStyles}
-        placeholder={props.placeholder}
+        placeholder={placeholder}
         type={'password'}
-        onChange={(e) => setInputVal(e.target.value)}
-        value={inputVal}
-        disabled={props.disabled}
+        onChange={(e) => {
+          onChanged && onChanged(e)
+        }}
+        value={value}
+        disabled={disabled}
       />
-    </div>
+    </Box>
   )
 }
 
 JGPassword.defaultProps = {
-  left: '0px',
-  top: '0px',
-  multiHeight: '26px',
-  multiWidth: '235px',
-  labelWidth: 94,
-  labelText: '文本',
+  labeltext: '文本',
+  width: '235px',
+  height: '26px',
+  position: 'absolute',
+  left: 0,
+  top: 0,
   placeholder: '',
-  isMust: false,
-  visible: true,
   labelVisible: true,
-  disabled: false
+  labelWidth: 94
 }
 
 export default JGPassword
-export { JGPassword }
-export type { JGPasswordProps }
