d362f223c5cb3196636dc633e00e3d056589216d
1589897392@qq.com <hubq@toone.com.cn>
2022-03-10 13:35:20 +0800

    '增加测试复杂度'

--- packages/utils/v-act-xml-parser/debug/index.js
+++ packages/utils/v-act-xml-parser/debug/index.js
@@ -8,13 +8,37 @@ const xml = `<root>
 	</define>
 	<sequence>
 		<evaluateRule code="BR_ShowMessage2"/>
+		<if>
+            <define>
+                <expression>False</expression>
+            </define>
+            <sequence>
+                <evaluateRule code="BR_ShowMessage6"/>
+            </sequence>
+        </if>
+        <else>
+            <sequence>
+                <foreach>
+                    <define>
+                        <varCode>var1</varCode>
+                        <entityType>window</entityType>
+                        <entityCode>ds</entityCode>
+                    </define>
+                    <sequence>
+                        <evaluateRule code="BR_ShowMessage7"/>
+                    </sequence>
+                </foreach>
+            </sequence>
+        </else>
 	</sequence>
 </if>
+
 <else>
 	<sequence>
 		<evaluateRule code="BR_ShowMessage3"/>
 	</sequence>
 </else>
+
 <foreach>
 	<define>
 		<varCode>var1</varCode>
@@ -25,7 +49,28 @@ const xml = `<root>
 		<evaluateRule code="BR_ShowMessage4"/>
 	</sequence>
 </foreach>
-<evaluateRule code="BR_ShowMessage5"/><root>`
+
+<if>
+    <define>
+        <expression>False</expression>
+    </define>
+    <sequence>
+        <evaluateRule code="BR_ShowMessage5"/>
+    </sequence>
+</if>
+
+<if>
+    <define>
+        <expression>True</expression>
+    </define>
+    <sequence>
+        <evaluateRule code="BR_ShowMessage9"/>
+    </sequence>
+</if>
+
+<evaluateRule code="BR_ShowMessage10"/>
+<root>
+`
 const dom = parser.parse(xml)
 
 parser.run(dom[0].children)
