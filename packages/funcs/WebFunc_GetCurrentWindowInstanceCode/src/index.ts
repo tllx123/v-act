6a3258715ec09d5b9656dccf4c4b902279ffb226
hwk394020931 <394020931@qq.com>
2022-03-10 17:17:39 +0800

    数据类型处理

--- packages/funcs/WebFunc_GetCurrentWindowInstanceCode/src/index.ts
+++ packages/funcs/WebFunc_GetCurrentWindowInstanceCode/src/index.ts
@@ -3,9 +3,10 @@
  *
  */
 import * as window from '@v-act/vjs.framework.extension.platform.services.integration.vds.window'
+
 const vds = { window }
 
-const main = function (param) {
+const main = function () {
   //		var currentWindowInstanceCode = scopeManager.getCurrentScopeId();//客户端方法里面执行会取到构件域
   //		return currentWindowInstanceCode ? currentWindowInstanceCode : null;
 
