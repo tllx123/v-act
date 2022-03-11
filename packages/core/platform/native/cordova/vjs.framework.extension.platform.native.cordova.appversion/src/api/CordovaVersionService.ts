cb6c5d1e3a93fbe5a6a133ffc28492bb3c81db57
zhougege520 <2912354876@qq.com>
2022-03-11 11:32:54 +0800

    VJS转换

--- packages/core/platform/native/cordova/vjs.framework.extension.platform.native.cordova.appversion/src/api/CordovaVersionService.ts
+++ packages/core/platform/native/cordova/vjs.framework.extension.platform.native.cordova.appversion/src/api/CordovaVersionService.ts
@@ -1,6 +1,6 @@
 import { VersionService } from '@v-act/vjs.framework.extension.platform.services.native.mobile.appversion'
 
-export function initModule(sb) {
+export function initModule(sb: any) {
   VersionService.putInstance(exports)
 }
 
@@ -9,26 +9,30 @@ export function initModule(sb) {
  * @param {Object} cordova.getAppVersion 全局调用对象
  *
  */
+//@ts-ignore
 if (window.VJSBridge) {
+  //@ts-ignore
   window.VJSBridge.plugins.vplatform.AppVersion = cordova.getAppVersion
 }
 
-const getVersionNumber = function (callback) {
+const getVersionNumber = function (callback: Function) {
   if (typeof callback != 'function') {
     throw new Error('获取APP版本号服务传参错误!请指定一个回调函数')
     return
   }
-  cordova.getAppVersion.getVersionNumber(function (version) {
+  //@ts-ignore
+  cordova.getAppVersion.getVersionNumber(function (version: string) {
     callback(version)
   })
 }
 
-const getAppName = function (callback) {
+const getAppName = function (callback: Function) {
   if (typeof callback != 'function') {
     throw new Error('获取AP名称服务传参错误!请指定一个回调函数')
     return
   }
-  cordova.getAppVersion.getAppName(function (appName) {
+  //@ts-ignore
+  cordova.getAppVersion.getAppName(function (appName: string) {
     callback(appName)
   })
 }
