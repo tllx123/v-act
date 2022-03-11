7d73c903c8453b96d537917ef2e12afa57db5e32
zhouwenqiang961026 <1355248989@qq.com>
2022-03-11 09:03:38 +0800

    1

--- packages/funcs/WebFunc_SetAlias/src/index.ts
+++ packages/funcs/WebFunc_SetAlias/src/index.ts
@@ -1,6 +1,7 @@
 import * as math from '@v-act/vjs.framework.extension.platform.services.integration.vds.math'
 import  {FunctionContext} from '@v-act/vjs.framework.extension.platform.interface.function'
 import * as exception from '@v-act/vjs.framework.extension.platform.services.integration.vds.exception'
+import * as log from '@v-act/vjs.framework.extension.platform.services.integration.vds.log'
 const vds = { math,exception }
 
 const main = function (param:FunctionContext) {
@@ -35,7 +36,47 @@ const main = function (param:FunctionContext) {
   }
 }
 function HandleException(error_msg:string) {
-  var exception = vds.exception.newBusinessException(error_msg)
-  throw exception
-}
+  log.log(error_msg);
+  return false;
+  // 以下逻辑将使手机端前台页面弹出提示信息
+  // var exception = factory.create({
+  // 	"type": factory.TYPES.Dialog,
+  // 	"message": error_msg
+  // });
+  // throw exception;
+};
+/**
+	 * desc 判断入参字节长度是否小于40字节（UTF-8）
+	 * param 入参
+	 * */
+	function checkStringLenValid(param:any) {
+		var totalLength = 0,
+			i, charCode;
+
+		for (i = 0; i < param.length; i++) {
+			charCode = param.charCodeAt(i);
+			if (charCode < 0x007f)
+				totalLength = totalLength + 1;
+			else if ((0x0080 <= charCode) && (charCode <= 0x07ff))
+				totalLength += 2;
+			else if ((0x0800 <= charCode) && (charCode <= 0xffff))
+				totalLength += 3;
+		}
+
+		if (totalLength > 40)
+			return false
+		else
+			return true
+	};
+  /**
+	 * desc 判断入参是否符合规范，仅支持数字、字母、下划线、中文
+	 * param 入参
+	 * */
+	function checkParamValid(param:any) {
+		if (param === "")
+			return true
+
+		var reg = new RegExp("^[0-9a-zA-Z_\u4e00-\u9FA5]+$");
+		return reg.test(param)
+	};
 export { main }
