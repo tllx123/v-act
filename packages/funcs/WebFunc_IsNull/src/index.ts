0c28088096364196176bec2d669906573e3c80b9
yzc <123@123.com>
2022-03-11 15:50:07 +0800

    新增（函数组件 - TS判断）

--- packages/funcs/WebFunc_IsNull/src/index.ts
+++ packages/funcs/WebFunc_IsNull/src/index.ts
@@ -2,9 +2,10 @@
  *	空值处理
  */
 import * as object from '@v-act/vjs.framework.extension.platform.services.integration.vds.object'
+
 const vds = { object }
 
-const main = function (arg, defaultVal) {
+const main = function (arg: any, defaultVal: any) {
   return vds.object.isUndefOrNull(arg) ? defaultVal : arg
 }
 export { main }
