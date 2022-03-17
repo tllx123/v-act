a30ccd895f3bbac12d017e1530a7d73e24975e9f
onlylife59 <octonber@qq.com>
2022-03-17 15:54:06 +0800

    模块提交

--- packages/core/publish/window/render/smartclient/vjs.framework.extension.publish.window.render.smartclient.viewlib/src/initRuleSet.ts
+++ packages/core/publish/window/render/smartclient/vjs.framework.extension.publish.window.render.smartclient.viewlib/src/initRuleSet.ts
@@ -1,72 +1,75 @@
-import { run } from '../../../../../../../utils/v-act-xml-parser/src/index'
+import { run } from '@v-act/xml-parser'
 import { WindowRoute } from '@v-act/vjs.framework.extension.platform.data.storage.schema.route'
 import {
-  ruleSet,
   logicType,
   routeParamsSchema,
-  ruleInstanceSchema,
-  ruleInstancesSchema
+  ruleInstanceSchema
 } from './interfase/ruleSetInterFace'
 
-const getRuleInstances = (ruleInstances: ruleInstancesSchema): object => {
-  let instances = {}
-  const setInstances = (instance: ruleInstanceSchema): void => {
-    let {
-      ruleConfig,
-      $: {
-        instanceCode,
-        isEnabled,
-        ruleCode,
-        isNeedLog,
-        instanceName,
-        transactionType,
-        ruleName
-      }
-    } = instance
-
-    instances[instanceCode] = {
-      enable: isEnabled === 'True',
-      condition: '',
-      ruleCode: ruleCode,
-      instanceCode: instanceCode,
-      needLog: isNeedLog === 'True',
-      instanceName: instanceName,
-      transactionType: transactionType,
-      ruleName: ruleName,
-      inParams: ruleConfig
+const setInstances = (
+  instance: ruleInstanceSchema,
+  instances: { [key: string]: any }
+): void => {
+  let {
+    ruleConfig,
+    $: {
+      instanceCode,
+      isEnabled,
+      ruleCode,
+      isNeedLog,
+      instanceName,
+      transactionType,
+      ruleName
     }
+  } = instance
+
+  instances[instanceCode] = {
+    enable: isEnabled === 'True',
+    condition: '',
+    ruleCode: ruleCode,
+    instanceCode: instanceCode,
+    needLog: isNeedLog === 'True',
+    instanceName: instanceName,
+    transactionType: transactionType,
+    ruleName: ruleName,
+    inParams: ruleConfig
   }
+}
 
-  if (Array.isArray(ruleInstances.ruleInstance)) {
-    for (let instance of ruleInstances.ruleInstance) {
-      setInstances(instance)
-    }
-  } else {
-    setInstances(ruleInstances.ruleInstance)
+const getRuleInstances = (ruleInstances: Array<ruleInstanceSchema>): object => {
+  let instances = {}
+  for (let instance of ruleInstances) {
+    setInstances(instance, instances)
   }
   return instances
 }
 
-const renderRoute = (logic: logicType): routeParamsSchema => {
-  let routeParamsType: routeParamsSchema = {
-    handler: null,
-    ruleInstances: {},
-    transactionInfo: {}
-  }
+const parseLogic = (logic: logicType): routeParamsSchema | null => {
   if (logic['$'].type == 'client') {
-    routeParamsType = madeData(logic.ruleSets.ruleSet)
-  }
-
-  function madeData(ruleSet: ruleSet): routeParamsSchema {
-    let { ruleRoute } = ruleSet
-    return {
+    const ruleSet2 = logic.ruleSets.ruleSet
+    const ruleRoute = ruleSet2.ruleRoute
+    const ruleInstances = ruleSet2.ruleInstances.ruleInstance
+    const ruleInstanceList = Array.isArray(ruleInstances)
+      ? ruleInstances
+      : [ruleInstances]
+    if (logic.ruleInstances && logic.ruleInstances.ruleInstance) {
+      if (Array.isArray(logic.ruleInstances.ruleInstance)) {
+        logic.ruleInstances.ruleInstance.forEach((inst: ruleInstanceSchema) => {
+          ruleInstanceList.push(inst)
+        })
+      } else {
+        ruleInstanceList.push(logic.ruleInstances.ruleInstance)
+      }
+    }
+    let route = {
       handler: Array.isArray(ruleRoute['_']) ? run(ruleRoute['_']) : null,
-      ruleInstances: getRuleInstances(ruleSet.ruleInstances),
+      ruleInstances: getRuleInstances(ruleInstanceList),
       transactionInfo: {}
     }
+    return route
+  } else {
+    return null
   }
-
-  return routeParamsType
 }
 
 const addRoute = (
@@ -74,36 +77,27 @@ const addRoute = (
   windowCode: string,
   logic: logicType
 ) => {
-  let { handler, ruleInstances, transactionInfo } = renderRoute(logic)
-
-  let $addRoute = WindowRoute.addRoute
-
-  let routeParams = {
-    componentCode: componentCode,
-    windowCode: windowCode,
-    route: {
-      routeCode: logic.ruleSets.ruleSet['$'].code,
-      outputs: null,
-      transactionType: 'TRANSACTION',
-      handler: function (
-        ruleEngine: {
-          executeWithRouteCallback: (config: {
-            ruleCode: string
-            routeContext: any
-          }) => void
+  const result = parseLogic(logic)
+  if (result != null) {
+    let { handler, ruleInstances, transactionInfo } = result
+    let routeParams = {
+      componentCode,
+      windowCode,
+      route: {
+        routeCode: logic.ruleSets.ruleSet['$'].code,
+        outputs: null,
+        transactionType: 'TRANSACTION',
+        handler: function (ruleEngine: any, routeRuntime: any) {
+          handler && handler(ruleEngine, routeRuntime)
         },
-        routeRuntime: any
-      ) {
-        handler && handler(ruleEngine, routeRuntime)
-      },
-      variables: [],
-      inputs: null,
-      ruleInstances: ruleInstances,
-      transactionInfo: transactionInfo
+        variables: [],
+        inputs: null,
+        ruleInstances,
+        transactionInfo
+      }
     }
+    WindowRoute.addRoute(routeParams)
   }
-  console.log('添加route，结构：', routeParams)
-  $addRoute(routeParams)
 }
 
 const init = function (params: {
@@ -111,11 +105,8 @@ const init = function (params: {
   windowCode: string
   winDatas: { logics: { logic: logicType | logicType[] } }
 }) {
-  console.log('初始化')
-
   const { componentCode, windowCode, winDatas } = params
   let { logic } = winDatas.logics
-
   // 如果是数组，则添加多个Route
   if (Array.isArray(logic)) {
     for (let item of logic) {
