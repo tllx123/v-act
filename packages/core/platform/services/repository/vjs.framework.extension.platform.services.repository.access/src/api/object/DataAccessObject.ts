bcc9adb7f7f1bb9782ea9eb3a4066c89769060c5
onlylife59 <octonber@qq.com>
2022-03-23 16:17:37 +0800

    模块提交

--- packages/core/platform/services/repository/vjs.framework.extension.platform.services.repository.access/src/api/object/DataAccessObject.ts
+++ packages/core/platform/services/repository/vjs.framework.extension.platform.services.repository.access/src/api/object/DataAccessObject.ts
@@ -4,70 +4,87 @@
  * vjs名称：vjs.framework.extension.platform.services.repository.access<br/>
  * vjs服务名称：vjs.framework.extension.platform.services.repository.data.object<br/>
  */
-let DataAccessObject = function (dataProvider, modelSchema, command) {
+class DataAccessObject {
   /**
    * 远程数据源信息<Obj>
    */
-  this.dataProvider = dataProvider
+  dataProvider
   /**
    * 数据模型定义信息<Object>
    */
-  this.modelSchema = modelSchema
+  modelSchema
   /**
    * 数据操作类型及相关条件信息<Object>
    *
    * 属性type的枚举值为：query insert update delete
    */
-  this.command = command
-}
+  command
 
-/**
- *设置远程数据源信息
- * @param {Object} dataProvider 远程数据源信息
- */
-DataAccessObject.prototype.setDataProvider = function (dataProvider) {
-  this.dataProvider = dataProvider
-}
+  constructor(dataProvider: any, modelSchema: any, command: string) {
+    /**
+     * 远程数据源信息<Obj>
+     */
+    this.dataProvider = dataProvider
+    /**
+     * 数据模型定义信息<Object>
+     */
+    this.modelSchema = modelSchema
+    /**
+     * 数据操作类型及相关条件信息<Object>
+     *
+     * 属性type的枚举值为：query insert update delete
+     */
+    this.command = command
+  }
 
-/**
- *获取远程数据源信息
- * @return Object
- */
-DataAccessObject.prototype.getDataProvider = function () {
-  return this.dataProvider
-}
+  /**
+   *设置远程数据源信息
+   * @param {Object} dataProvider 远程数据源信息
+   */
+  setDataProvider(dataProvider: any) {
+    this.dataProvider = dataProvider
+  }
 
-/**
- *设置数据模型定义信息
- * @param {Object} modelSchema 数据模型定义信息
- */
-DataAccessObject.prototype.setModelSchema = function (setModelSchema) {
-  this.setModelSchema = setModelSchema
-}
+  /**
+   *获取远程数据源信息
+   * @return Object
+   */
+  getDataProvider() {
+    return this.dataProvider
+  }
 
-/**
- *获取数据模型定义信息
- * @return Object
- */
-DataAccessObject.prototype.getModelSchema = function () {
-  return this.modelSchema
-}
+  /**
+   *设置数据模型定义信息
+   * @param {Object} modelSchema 数据模型定义信息
+   */
+  setModelSchema(setModelSchema: any) {
+    this.setModelSchema = setModelSchema
+  }
 
-/**
- *设置数据操作类型及相关条件信息
- * @param {String} command 操作类型
- * 枚举值：query、insert、update、delete
- */
-DataAccessObject.prototype.setCommand = function (command) {
-  this.command = command
-}
+  /**
+   *获取数据模型定义信息
+   * @return Object
+   */
+  getModelSchema() {
+    return this.modelSchema
+  }
 
-/**
- *获取数据操作类型及相关条件信息
- * @return String
- */
-DataAccessObject.prototype.getCommand = function () {
-  return this.command
+  /**
+   *设置数据操作类型及相关条件信息
+   * @param {String} command 操作类型
+   * 枚举值：query、insert、update、delete
+   */
+  setCommand(command: string) {
+    this.command = command
+  }
+
+  /**
+   *获取数据操作类型及相关条件信息
+   * @return String
+   */
+  getCommand() {
+    return this.command
+  }
 }
 
 export default DataAccessObject
