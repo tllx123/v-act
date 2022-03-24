0a7bae9e4185b27446d7f89b45f937ed8685ba7a
三分归元气 <1801520013@qq.com>
2022-03-24 17:47:42 +0800

    临时提交

--- packages/core/platform/interface/vjs.framework.extension.platform.interface.model.datasource/src/index.ts
+++ packages/core/platform/interface/vjs.framework.extension.platform.interface.model.datasource/src/index.ts
@@ -1,4 +1,7 @@
 export * as Criteria from './api/Criteria'
 export * as Record from './api/Record'
+import ResultSet from './api/ResultSet'
 export * as Datasource from './api/Datasource'
 export * as DatasourceFactory from './api/DatasourceFactory'
+
+export { ResultSet }
