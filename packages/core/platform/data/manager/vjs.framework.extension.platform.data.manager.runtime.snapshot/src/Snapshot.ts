06b2697bb6ce785322a69623066157a42ed3b641
三分归元气 <1801520013@qq.com>
2022-03-24 14:21:45 +0800

    js转ts

--- packages/core/platform/data/manager/vjs.framework.extension.platform.data.manager.runtime.snapshot/src/Snapshot.ts
+++ packages/core/platform/data/manager/vjs.framework.extension.platform.data.manager.runtime.snapshot/src/Snapshot.ts
@@ -1,22 +1,24 @@
 import { uuid } from '@v-act/vjs.framework.extension.util.uuid'
 
 let Snapshot = function () {
+  // @ts-ignore
   this.id = uuid.generate()
+  // @ts-ignore
   this.datasourceSnapshot = {}
 }
 
 Snapshot.prototype = {
-  initModule: function (sb) {},
+  initModule: function (sb: any) {},
 
   getId: function () {
     return this.id
   },
 
-  registerDatsourceSnapshot: function (datasourceName, snapshot) {
+  registerDatsourceSnapshot: function (datasourceName: string, snapshot: any) {
     this.datasourceSnapshot[datasourceName] = snapshot
   },
 
-  getDatasourceSnapshot: function (datasourceName) {
+  getDatasourceSnapshot: function (datasourceName: string) {
     return this.datasourceSnapshot[datasourceName]
   }
 }
