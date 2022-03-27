716e48840c7c44a34385a510aa49ee7e3da9023b
hwk394020931 <394020931@qq.com>
2022-03-27 17:18:03 +0800

    代码提交

--- packages/core/platform/services/integration/vjs.framework.extension.platform.services.integration.server.ruleset/src/api/IntegrationServerRuleSet.ts
+++ packages/core/platform/services/integration/vjs.framework.extension.platform.services.integration.server.ruleset/src/api/IntegrationServerRuleSet.ts
@@ -1,19 +1,15 @@
-let _instance
+let _instance: Record<string, any>
 
-const _putInstance = function (instance) {
+const _putInstance = function (instance: Record<string, any>) {
   _instance = instance
 }
 
-const invoke = function (params) {
+const invoke = function (params: any) {
   _instance.invoke(params)
 }
 
-const invoke = function (params) {
-  _instance.invoke(params)
-}
-
-const invokeV3Webapi = function (params) {
+const invokeV3Webapi = function (params: any) {
   _instance.invokeV3Webapi(params)
 }
 
-export { _putInstance, invoke, invoke, invokeV3Webapi }
+export { _putInstance, invoke, invokeV3Webapi }
